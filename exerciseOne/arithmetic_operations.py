f4d33595761d92c0252c0c453a4e86cc20a89be8
Dimitar Haralampiev <dimitur.haralampiev@gmail.com>
2023-10-10 21:39:03 +0300

    feat: Add second exercise

---
+++ exerciseOne/arithmetic_operations.py
@@ -0,0 +1,14 @@
+number_one = 5
+number_two = 15
+
+result_add = number_one + number_two
+print(result_add)
+
+result_multiply = number_one * number_two
+print(result_multiply)
+
+result_sub = number_two - number_one
+print(result_sub)
+
+result_div = number_two / number_one
+print(result_div)
