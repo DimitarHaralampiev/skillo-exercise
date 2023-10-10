80b1c0115e56af998d290aa97ae5d66c1d880819
Dimitar Haralampiev <dimitur.haralampiev@gmail.com>
2023-10-10 21:32:06 +0300

    feat: Add first exercise

---
+++ exerciseOne/dict_vowel_name.py
@@ -0,0 +1,16 @@
+name_one = ('Ivan', 'Ivanov')
+name_two = ('Petar', 'Petrov')
+name_three = ('Dimitar', 'Dimitrov')
+name_four = ('Stefan', 'Stefanov')
+name_five = ('Ivelina', 'Ivanova')
+
+egn_one = ('8888822222')
+egn_two = ('9999977777')
+egn_three = ('9999933333')
+egn_four = ('5555522222')
+egn_five = ('9999966666')
+
+dict_egn_name = {
+    name_one: egn_one,
+    name_five: egn_five,
+}
