02b803188601f235384f34672e22b4e6cfeb353f
Dimitar Haralampiev <dimitur.haralampiev@gmail.com>
2023-10-19 00:57:53 +0300

    refactor: Refactor methods

--- exerciseThree/online_shop.py
+++ exerciseThree/online_shop.py
@@ -1,67 +1,44 @@
-# Global dictionary to store shopping carts
-shopping_carts = {}
+class ShoppingCart:
+    def __init__(self):
+        self.shopping_carts = {}
+
+    def add_item(self, customer_id, item, price):
+        if customer_id in self.shopping_carts:
+            self.shopping_carts[customer_id].append((item, price))
+        else:
+            self.shopping_carts[customer_id] = [(item, price)]
+
+    def remove_item(self, customer_id, item):
+        if customer_id in self.shopping_carts:
+            shopping_cart = self.shopping_carts[customer_id]
+            for i, (cart_item, price) in enumerate(shopping_cart):
+                if cart_item == item:
+                    shopping_cart.pop(i)
+                    return f'Removed item {item}'
+        return f'{customer_id} does not exist'
+
+    def calculate_total(self, customer_id):
+        if customer_id in self.shopping_carts:
+            total_price = sum(item[1] for item in self.shopping_carts[customer_id])
+            return f'Total price for {customer_id} cart: {total_price}lv.'
+        return f'{customer_id} does not exist'
+
+    def display_cart(self, customer_id):
+        if customer_id in self.shopping_carts:
+            shopping_cart = self.shopping_carts[customer_id]
+            if not shopping_cart:
+                print(f'{customer_id} cart is empty.')
+            print(f'{customer_id} Shopping Cart:')
+            for (item, price) in shopping_cart:
+                print(f'{item}: {price}lv.')
+            print(self.calculate_total(customer_id))
+        else:
+            print(f'{customer_id} does not exist')
 
 
-# Function to add items to the cart
-def add_item(customer_id, item, price):
-    # If the customer already has a cart, append the new item.
-    # Otherwise, create a new cart with the item.
-    if customer_id in shopping_carts:
-        shopping_carts[customer_id].append((item, price))
-    else:
-        shopping_carts[customer_id] = [(item, price)]
+shopping_cart = ShoppingCart()
 
-
-# Function to remove items from the cart
-def remove_item(customer_id, item):
-    # Ensure the customer exists.
-    check_customer(customer_id)
-
-    # Get the customer's shopping cart.
-    shopping_cart = shopping_carts[customer_id]
-
-    # Search for the item to remove and remove it if found.
-    shopping_cart = shopping_carts[customer_id]
-    for i, (cart_item, price) in enumerate(shopping_cart):
-        if cart_item == item:
-            shopping_cart.pop(i)
-            return f'Removed item {item}'
-
-
-# Function to calculate the total price of items in the cart
-def calculate_total(customer_id):
-    # Ensure the customer exists.
-    check_customer(customer_id)
-
-    # Calculate the total price of items in the cart.
-    total_price = sum(item[1] for item in shopping_carts[customer_id])
-    return f'Total price for {customer_id} cart: {total_price}lv.'
-
-
-# Function to display the contents of the cart
-def display_cart(customer_id):
-    # Ensure the customer exists.
-    check_customer(customer_id)
-
-    # Get the customer's shopping cart.
-    shopping_cart = shopping_carts[customer_id]
-
-    # Check if the cart is empty and print its contents.
-    if not shopping_cart:
-        print(f'{customer_id} cart is empty.')
-
-    print(f'{customer_id} Shopping Cart:')
-    for (item, price) in shopping_cart:
-        print(f'{item}: {price}lv.')
-    print(calculate_total(customer_id))
-
-
-def check_customer(customer_id):
-    if customer_id not in shopping_carts:
-        raise ValueError(f'{customer_id} does not in shopping cart')
-
-
-choice = input('Enter your choice: ')
+choice = input('Enter your choice (Add/Remove/Display/Stop): ')
 
 while choice != 'Stop':
 
@@ -69,14 +46,16 @@ while choice != 'Stop':
         customer_id = input('Enter customer ID: ')
         item = input('Enter item name: ')
         price = float(input('Enter item price: '))
-        add_item(customer_id, item, price)
+        shopping_cart.add_item(customer_id, item, price)
     elif choice == 'Remove':
         customer_id = input('Enter customer ID: ')
         item = input('Enter item to remove: ')
-        print(remove_item(customer_id, item))
+        print(shopping_cart.remove_item(customer_id, item))
     elif choice == 'Display':
         customer_id = input('Enter customer ID: ')
-        display_cart(customer_id)
+        shopping_cart.display_cart(customer_id)
+    else:
+        print('Invalid choice. Please choose Add, Remove, Display, or Stop.')
 
     choice = input('Enter your choice: ')
 
--- exerciseThree/simple_calculator.py
+++ exerciseThree/simple_calculator.py
@@ -12,7 +12,10 @@ def simple_calculator(first_num, second_num, operator):
     return operator_functions[operator](first_num, second_num)
 
 
-op = input('Please enter operator')
-number_one = 5.5
-number_two = 24
-print(simple_calculator(number_one, number_two, op))
+op = input('Please enter operator.Possible operators (+, -, *, /) ')
+number_one = float(input('Please enter first Number '))
+number_two = float(input('Please enter second Number '))
+
+while op != 'Stop':
+    print(f'{number_one} {op} {number_two} = {simple_calculator(number_one, number_two, op)}')
+    op = input('Please enter operator.Possible operators (+, -, *, /) ')
