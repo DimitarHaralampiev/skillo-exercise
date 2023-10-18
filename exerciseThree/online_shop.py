41d635fe6ce93d41b973c1f7171541896a7829f2
Dimitar Haralampiev <dimitur.haralampiev@gmail.com>
2023-10-18 22:59:20 +0300

    reformat: Reformat code

--- exerciseThree/online_shop.py
+++ exerciseThree/online_shop.py
@@ -1,43 +1,64 @@
+# Global dictionary to store shopping carts
 shopping_carts = {}
 
 
+# Function to add items to the cart
 def add_item(customer_id, item, price):
+    # If the customer already has a cart, append the new item.
+    # Otherwise, create a new cart with the item.
     if customer_id in shopping_carts:
         shopping_carts[customer_id].append((item, price))
     else:
         shopping_carts[customer_id] = [(item, price)]
 
 
+# Function to remove items from the cart
 def remove_item(customer_id, item):
-    if customer_id not in shopping_carts:
-        return f'{customer_id} not found in {shopping_carts}.'
+    # Ensure the customer exists.
+    check_customer(customer_id)
 
+    # Get the customer's shopping cart.
+    shopping_cart = shopping_carts[customer_id]
+
+    # Search for the item to remove and remove it if found.
     shopping_cart = shopping_carts[customer_id]
     for i, (cart_item, price) in enumerate(shopping_cart):
         if cart_item == item:
             shopping_cart.pop(i)
-            return f'Remove item {item}'
+            return f'Removed item {item}'
 
 
+# Function to calculate the total price of items in the cart
 def calculate_total(customer_id):
-    if customer_id not in shopping_carts:
-        return f'{customer_id} does not have a shopping cart.'
+    # Ensure the customer exists.
+    check_customer(customer_id)
 
-    shopping_cart = shopping_carts[customer_id]
-    total = sum(price for _, price in shopping_cart)
-    return f'Total price for {customer_id} is cart: {total}lv.'
+    # Calculate the total price of items in the cart.
+    total_price = sum(item[1] for item in shopping_carts[customer_id])
+    return f'Total price for {customer_id} cart: {total_price}lv.'
 
 
+# Function to display the contents of the cart
 def display_cart(customer_id):
-    if customer_id not in shopping_carts:
-        raise ValueError(f'{customer_id} does not have a shopping cart.')
+    # Ensure the customer exists.
+    check_customer(customer_id)
 
+    # Get the customer's shopping cart.
     shopping_cart = shopping_carts[customer_id]
-    if not shopping_cart:
-        raise ValueError(f'{customer_id} is cart is empty.')
 
+    # Check if the cart is empty and print its contents.
+    if not shopping_cart:
+        print(f'{customer_id} cart is empty.')
+
+    print(f'{customer_id} Shopping Cart:')
     for (item, price) in shopping_cart:
         print(f'{item}: {price}lv.')
+    print(calculate_total(customer_id))
+
+
+def check_customer(customer_id):
+    if customer_id not in shopping_carts:
+        raise ValueError(f'{customer_id} does not in shopping cart')
 
 
 choice = input('Enter your choice: ')
@@ -52,11 +73,7 @@ while choice != 'Stop':
     elif choice == 'Remove':
         customer_id = input('Enter customer ID: ')
         item = input('Enter item to remove: ')
-        remove_item(customer_id, item)
-    elif choice == 'Calc':
-        customer_id = input('Enter customer ID: ')
-        total = calculate_total(customer_id)
-        print(f'Total price for {customer_id} is cart: {total}lv.')
+        print(remove_item(customer_id, item))
     elif choice == 'Display':
         customer_id = input('Enter customer ID: ')
         display_cart(customer_id)
