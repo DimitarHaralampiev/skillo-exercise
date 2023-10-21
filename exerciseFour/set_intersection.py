f0d0d390afd8c36e44bde820a21476408d899428
Dimitar Haralampiev <dimitur.haralampiev@gmail.com>
2023-10-21 11:40:23 +0300

    Add exercise ten

--- exerciseFour/set_intersection.py
+++
@@ -1,7 +0,0 @@
-def intersection(set_one, set_two):
-    return set_one.intersection(set_two)
-
-
-set_one = set([x for x in input('Please enter item ').split() if x != 'Stop'])
-set_two = set([x for x in input('Please enter item ').split() if x != 'Stop'])
-print(intersection(set_one, set_two))
